47ff71d078489d093745f5c928de70cbc2d4e1a0
EltonBecky <drkbz5787061@outlook.com>
2025-11-14 11:45:00 +0000

    perf: optimize Solidity compilation settings for better gas efficiency
    - Increase optimizer runs to 1000 for better bytecode optimization
    - Enable viaIR compilation for improved gas performance
    - Reduce contract deployment and execution costs for athlete registration

---
+++ hardhat.config.js
@@ -0,0 +1,86 @@
+require("@fhevm/hardhat-plugin");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomicfoundation/hardhat-ethers");
+require("@nomicfoundation/hardhat-verify");
+require("@typechain/hardhat");
+require("hardhat-deploy");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+
+require("./tasks/accounts");
+require("./tasks/AthleteRegistration");
+
+// Run 'npx hardhat vars setup' to see the list of variables that need to be set
+
+const MNEMONIC = vars.get("MNEMONIC", "test test test test test test test test test test test junk");
+const INFURA_API_KEY = vars.get("INFURA_API_KEY", "b18fb7e6ca7045ac83c41157ab93f990");
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x6997ba8119ffba8c5133e76a945a145b2e3f02eb98ffcfd9c81cfd2cd72f8216";
+
+const config = {
+  defaultNetwork: "hardhat",
+  namedAccounts: {
+    deployer: 0, // Use first account from accounts array
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: vars.get("ETHERSCAN_API_KEY", ""),
+    },
+  },
+  gasReporter: {
+    currency: "USD",
+    enabled: process.env.REPORT_GAS ? true : false,
+    excludeContracts: [],
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: MNEMONIC,
+      },
+      chainId: 31337,
+    },
+    anvil: {
+      accounts: {
+        mnemonic: MNEMONIC,
+        path: "m/44'/60'/0'/0/",
+        count: 10,
+      },
+      chainId: 31337,
+      url: "http://localhost:8545",
+    },
+    sepolia: {
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+    },
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  solidity: {
+    version: "0.8.27",
+    settings: {
+      metadata: {
+        // Not including the metadata hash
+        // https://github.com/paulrberg/hardhat-template/issues/31
+        bytecodeHash: "none",
+      },
+      // Disable the optimizer when debugging
+      // https://hardhat.org/hardhat-network/#solidity-optimizer-support
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+      viaIR: true,
+      evmVersion: "cancun",
+    },
+  },
+  typechain: {
+    outDir: "types",
+    target: "ethers-v6",
+  },
+};
+
+module.exports = config;
